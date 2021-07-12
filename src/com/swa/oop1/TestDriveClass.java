70eb52919e0576bd29dbaa4775b8df51fa2bafa7
piyatida646kp <80673288+piyatida646kp@users.noreply.github.com>
2021-07-12 15:03:03 +0700

    rename variable and method

--- src/com/swa/oop1/TestDriveClass.java
+++ src/com/swa/oop1/TestDriveClass.java
@@ -3,6 +3,6 @@ package com.swa.oop1;
 public class TestDriveClass {
 	 public static void main(String[] args) {
 	        Product p = new Product("P0001","Nintendo Switch");
-	        System.out.println(p.getDetailProduct());
+	        System.out.println(p.getDetail());
 	    }
 }
